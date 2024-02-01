82e8267163bcc2d2ece1ce2b85ab08565e37ff84
Mariola <mariolahullings@gmail.com>
2024-02-01 14:56:18 -0500

    Display items inside a SwiftUI list view

--- H4XOR News/ContentView.swift
+++ H4XOR News/ContentView.swift
@@ -10,16 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-        List {
+            List(posts) {
+                post in Text(post.title)
+            }
             Text("Hello, world!")
-            Text("Good bye, world!")
-            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
+//            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
         }
         .navigationTitle("H4XOR NEWS")
         }
     }
-}
 
 #Preview {
     ContentView()
 }
+
+struct Post: Identifiable {
+let id: String
+let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Hi"),
+    Post(id: "3", title: "Hey")
+]
