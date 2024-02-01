4a9ced57ad279c81ddc131ed514025aff3e53f2a
Mariola <mariolahullings@gmail.com>
2024-02-01 15:33:39 -0500

    Created  network call

---
+++ H4XOR News/Models/NetworkManager.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkManager.swift
+//  H4XOR News
+//
+//  Created by Mariola Hullings on 2/1/24.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            //starts off the networking task
+            task.resume()
+        }
+    }
+}
---
+++ H4XOR News/Models/PostData.swift
@@ -0,0 +1,24 @@
+//
+//  PostData.swift
+//  H4XOR News
+//
+//  Created by Mariola Hullings on 2/1/24.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+//Identifiable always has an id property
+//need identifiable protocol in order to use that structure (post in Text(post.title) inside the list, and for the to know how to order each item.
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectId
+    }
+    let objectId: String
+    let points: Int
+    let title: String
+    let url: String
+}
