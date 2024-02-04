150a853d7986b9bf28484d1373284d01c5c7b12d
Mariola <mariolahullings@gmail.com>
2024-02-04 08:56:15 -0500

    Created a WebView

--- H4XOR News/Views/WebView.swift
+++ H4XOR News/Views/WebView.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    //When make UIView, it's going to try and create a UIKit WebView and turn it into a SwiftUI WebView.
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
